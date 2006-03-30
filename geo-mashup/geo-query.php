43bc34630820c603805c54df708a2c3aba0ad4fe
cyberhobo <cyberhobo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-03-30 21:40:04 +0000

    Updating to use $_GET instead of relying on register_globals.
    git-svn-id: https://plugins.svn.wordpress.org/geo-mashup/trunk@5698 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geo-mashup/geo-query.php
+++ geo-mashup/geo-query.php
@@ -12,22 +12,27 @@ $query_string = 'SELECT post_id, meta_value'.
 	' WHERE meta_key=\'_geo_location\''.
 	' AND length(meta_value)>1';
 
+$minlat = $_GET['minlat'];
 if (is_numeric($minlat)) {
 	$minlat = mysql_real_escape_string($minlat);
 	$query_string .= " AND substring_index(meta_value,',',1)>$minlat";
 }
+$minlon = $_GET['minlon'];
 if (is_numeric($minlon)) {
 	$minlon = mysql_real_escape_string($minlon);
 	$query_string .= " AND substring_index(meta_value,',',-1)>$minlon";
 }
+$maxlat = $_GET['maxlat'];
 if (is_numeric($maxlat)) {
 	$maxlat = mysql_real_escape_string($maxlat);
 	$query_string .= " AND substring_index(meta_value,',',1)<$maxlat";
 }
+$maxlon = $_GET['maxlon'];
 if (is_numeric($maxlon)) {
 	$maxlon = mysql_real_escape_string($maxlon);
 	$query_string .= " AND substring_index(meta_value,',',-1)<$maxlon";
 }
+$category = $_GET['category'];
 if ($category) {
 	$category = mysql_real_escape_string($category);
 	$query_string .= " AND category_nicename='$category'";
@@ -35,6 +40,7 @@ if ($category) {
 
 $query_string .= " ORDER BY post_id DESC";
 
+$limit = $_GET['limit'];
 if (!($minlat && $maxlat && $minlon && $maxlon) && !$limit && !$category) {
 	// limit is not geographic, so limit number of results
 	$query_string .= " LIMIT 0,10";
