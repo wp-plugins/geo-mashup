ad785234088ea1cbdcc350d17afebed97f5b08b8
cyberhobo <cyberhobo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-09 21:58:19 +0000

    deploy from build of 1.5.1
    git-svn-id: https://plugins.svn.wordpress.org/geo-mashup/trunk@665720 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( basename( dirname( dirname( __FILE__ ) ) ) . '/geo-mashup.php' ),
-);
-
-require dirname( __FILE__ ) . '/lib/bootstrap.php';
-
